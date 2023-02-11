da532a7d3af742325bf1632dbadca179e2e9ddad
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2023-02-11 21:32:05 +0800

    Styled the header and body.

--- src/App.jsx
+++ src/App.jsx
@@ -39,16 +39,36 @@ function App() {
       <div className="App-Header">
         <h1 className="App-Header-Title">pomodoro</h1>
         <ul className="App-Header-Modes">
-          <li onClick={()=>handleTimerChange(0)}>Pomodoro</li>
-          <li onClick={()=>handleTimerChange(1)}>Short Break</li>
-          <li onClick={()=>handleTimerChange(2)}>Long Break</li>
+          <li className={timerMode == 0 && "Mode-Active"} onClick={()=>handleTimerChange(0)}>Pomodoro</li>
+          <li className={timerMode == 1 && "Mode-Active"} onClick={()=>handleTimerChange(1)}>Short Break</li>
+          <li className={timerMode == 2 && "Mode-Active"} onClick={()=>handleTimerChange(2)}>Long Break</li>
         </ul>
       </div>
       <div className="App-Body">
-        <div className="App-Timer">
-          {(Math.floor(pomoTimer/60)).toLocaleString('en-US', {minimumIntegerDigits: 2,useGrouping: false})+":"+(pomoTimer%60).toLocaleString('en-US', {minimumIntegerDigits: 2,useGrouping: false})}
+        <div className="App-Body-Timer">
+          <div class="App-Body-Timer-Ring">
+            <svg class="App-Body-Timer-Ring-SVG" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <g class="App-Body-Timer-Ring-Circle">
+                <circle class="App-Body-Timer-Ring-PathElapsed" cx="50" cy="51.5" r="42" />
+                <path
+                  id="base-timer-path-remaining"
+                  stroke-dasharray="283"
+                  class={"App-Body-Timer-Ring-Remaining"}
+                  d="
+                  M 50, 50
+                  m -45, 0
+                  a 45,45 0 1,0 90,0
+                  a 45,45 0 1,0 -90,0
+                  "
+                ></path>
+              </g>
+            </svg>     
+          </div>
+          <div className="App-Body-Timer-Display">
+            {(Math.floor(pomoTimer/60)).toLocaleString('en-US', {minimumIntegerDigits: 2,useGrouping: false})+":"+(pomoTimer%60).toLocaleString('en-US', {minimumIntegerDigits: 2,useGrouping: false})}
+          </div>
+          <button onClick={()=>{setPause(!pause)}} className="App-Timer-Pause">Pause</button>
         </div>
-        <button onClick={()=>{setPause(!pause)}} className="App-Timer-Pause">Pause</button>
       </div>
     </div>
   )
